33920674e4509b648d7709d9f2954fcb1738b2fb
Kimon Cokona <kimon.cokona@tum.de>
2020-01-03 19:02:07 +0100

    BrokenPipe and Multithread files added

--- Project/simple_trials_with_echo_servers/forward_server_multithread.py
+++ Project/simple_trials_with_echo_servers/forward_server_multithread.py
@@ -3,33 +3,24 @@ import logging
 import threading
 import time
 
-def create_client(ip_address, tcp_port, message=None, conn_client=None, addr_client=None):
+def create_client(ip_address, tcp_port, message=None):
     '''
     Creates a client that sends a TCP connection request to the given ip address and port
         ip_address: string
         tcp_port: integer
     '''
-    if not conn_client:
-        print('Client establishing new connection with ryu')
-        HOST = ip_address
-        PORT = tcp_port
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((HOST, PORT))
-            conn, addr = s.getsockname()
-            s.sendall(message)
-            data = s.recv(1024)
-            return data, conn, addr
-    else:
-        print('Client re-establishing old connection with ryu')
-        HOST = conn_client
-        PORT = addr_client
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((HOST, PORT))
-            s.sendall(message)
-            data = s.recv(1024)
-            return data
-    # print('Received', repr(data))
-
+    HOST = ip_address
+    PORT = tcp_port
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        '''
+        socket.AF_INET      -->     IPv4
+        socket.SOCK_STREAM  -->     TCP
+        '''
+        s.connect((HOST, PORT))
+        s.sendall(message)
+        data = s.recv(1024)
+        # print('Received', repr(data))
+    return data
 
 def create_server(ip_address=None, tcp_port=None):
     '''
@@ -42,12 +33,11 @@ def create_server(ip_address=None, tcp_port=None):
         PORT = tcp_port
     else:
         HOST = '127.0.0.1'      # Standard loopback interface address (localhost)
-        PORT = 65434                # Port to listen on (non-privileged ports are > 1023)
+        PORT = 65433                # Port to listen on (non-privileged ports are > 1023)
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         while True:
-            conn_client, addr_client = None, None
             s.listen()
             conn, addr = s.accept()
             with conn:
@@ -57,7 +47,7 @@ def create_server(ip_address=None, tcp_port=None):
                     # print('Data received ' + str(data))
                     if not data:
                         break
-                    ryu_reaction_data, conn_client, addr_client = create_client(ip_address='127.0.0.1', tcp_port=6633, message=data, conn_client=conn_client, addr_client=addr_client)
+                    ryu_reaction_data = create_client(ip_address='127.0.0.1', tcp_port=6633, message=data)
                     conn.sendall(ryu_reaction_data)
                     # print('Message has been forwarded')
 
@@ -69,6 +59,28 @@ def create_server(ip_address=None, tcp_port=None):
 #     logging.info("Main    : before creating thread")
 #     server1 = threading.Thread(target=create_server, args=(server1, ))
 #     server2 = threading.Thread(target=client_function, args=(2,))
+
+# if __name__ == "__main__":
+#     format = "%(asctime)s: %(message)s"
+#     logging.basicConfig(format=format, level=logging.INFO,
+#                         datefmt="%H:%M:%S")
+
+#     host = ''
+#     port = 65432
+#     action = 'search'
+#     value = 'morpheus'
+
+#     logging.info("Main    : before creating thread")
+#     x = threading.Thread(target=server_function, args=(1,))
+#     y = threading.Thread(target=client_function, args=(2,))
+
+#     logging.info("Main    : before running thread")
+#     x.start()
+#     y.start()
+#     logging.info("Main    : wait for the thread to finish")
+#     # x.join()
+#     logging.info("Main    : all done")
+
 
 
 create_server()
