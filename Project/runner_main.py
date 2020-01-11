65fdf917c5fdfbe3f2492e2799bcfbf510b1d6d2
ge73vum <duan.shen@tum.de>
2020-01-11 16:34:58 +0000

    new runner

---
+++ Project/runner_main.py
@@ -0,0 +1,15 @@
+from subprocess import Popen
+import sys
+
+if __name__ == '__main__':
+    try:
+        Popen(['gnome-terminal', '-e', "ryu-manager --observe-links Project/Controllers/simple_switch_13.py"])
+        #Popen(['gnome-terminal', '-e', "python3 Project/Hypervisors/TCP_proxy.py"])
+        Popen(['gnome-terminal', '-e', "python3 Project/Hypervisors/Hypervisor_main.py"])
+        Popen(['gnome-terminal', '-e', "sudo mn --custom Project/Topologies/sdn_topo_2.py --topo=mytopo --controller=remote,ip=127.0.0.1,port=65432,protocols=OpenFlow13"])
+    except KeyboardInterrupt:
+        print("Ctrl C - Stopping server")
+        sys.exit(1)
+    except:
+        print("There is an error with the runner")
+
