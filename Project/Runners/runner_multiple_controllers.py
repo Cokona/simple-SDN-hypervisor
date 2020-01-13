d85161fd5b4b2652abe976b2ad9b0a9fefe20bf2
Kimon Cokona <kimon.cokona@tum.de>
2020-01-13 14:26:28 +0100

    Runners re located

--- Project/Runners/runner_multiple_controllers.py
+++ Project/Runners/runner_multiple_controllers.py
@@ -4,8 +4,8 @@ import sys
 if __name__ == '__main__':
     try:
         call(['gnome-terminal', '-e', "ryu-manager --observe-links Project/Controllers/simple_switch_13.py"])
-        call(['gnome-terminal', '-e', "ryu-manager --ofp-tcp-listen-port 6644 --observe-links Project/Controllers/simple_switch_13.py"])
-        call(['gnome-terminal', '-e', "python3 Project/Hypervisors/TCP_proxy_multiple_controllers.py"])
+        call(['gnome-terminal', '-e', "ryu-manager --ofp-tcp-listen-port 6643 --observe-links Project/Controllers/simple_switch_13.py"])
+        call(['gnome-terminal', '-e', "python3 Project/Hypervisors/TCP_proxy_multiple_controllers_robust.py"])
         call(['gnome-terminal', '-e', "sudo mn --custom Project/Topologies/sdn_topo_3.py --topo=mytopo --controller=remote,ip=127.0.0.1,port=65432,protocols=OpenFlow13"])
         # call(['gnome-terminal', '-e', "sudo mn --controller=remote,ip=127.0.0.1,port=65432,protocols=OpenFlow13"])      
 
