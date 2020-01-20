2ffd02f4a4dfac7de4440bfb6dfa3e534a531a7c
Duan Shen <duan.shen@tum.de>
2020-01-20 16:24:24 +0100

    Replace tabs.py

--- Project/Hypervisors/tabs.py
+++ Project/Hypervisors/tabs.py
@@ -4,7 +4,7 @@ from tkinter.ttk import Notebook,Entry
 def insert_slice_rows(slice_no): #slice_no = 1, 2, 3, ...
     label_slice = Label(tab2, text="Slice " + str(slice_no), bg="#0059b3", fg="white", padx=3,pady=3)
     label_slice.config(font=('Arial', 12))
-    label_slice.grid(row=3*(slice_no-1)+1, column=0, sticky="nsew", columnspan=1, rowspan=3, padx=1, pady=1)
+    label_slice.grid(row=3*(slice_no-1)+1, column=0, sticky="nsew", columnspan=1, rowspan=3, padx=1, pady=(1,3))
 
     label_ports = Label(tab2, text="ports", bg="white", fg="black", padx=3,pady=3)
     label_ports.config(font=('Arial', 12))
@@ -26,12 +26,12 @@ def insert_slice_rows(slice_no): #slice_no = 1, 2, 3, ...
 
     label_n_flows = Label(tab2, text="# flow entries", bg="white", fg="black", padx=3, pady=3)
     label_n_flows.config(font=('Arial', 12))
-    label_n_flows.grid(row=3 + 3 * (slice_no - 1), column=1, sticky="nsew", columnspan=1, rowspan=1, padx=1, pady=1)
+    label_n_flows.grid(row=3 + 3 * (slice_no - 1), column=1, sticky="nsew", columnspan=1, rowspan=1, padx=1, pady=(1,3))
 
     for i in range(2, 2 + n_switches):
         label_cell = Label(tab2, text="", bg="white", fg="black", padx=3, pady=3)
         label_cell.config(font=('Arial', 12))
-        label_cell.grid(row=3 + 3 * (slice_no - 1), column=i, sticky="nsew", columnspan=1, rowspan=1, padx=1, pady=1)
+        label_cell.grid(row=3 + 3 * (slice_no - 1), column=i, sticky="nsew", columnspan=1, rowspan=1, padx=1, pady=(1,3))
     pass
 
 def getValue(value):
@@ -64,16 +64,16 @@ tab2.pack(fill="both")
 n_slices = 3
 n_switches = 4
 #adding table into tab2
-label_sw_no = Label(tab2, text="Slice \ Switch", bg="#0059b3", fg="white", padx=3,pady=3) #, anchor = "n"
-label_sw_no.config(font=('Arial',12))
-label_sw_no.grid(row=0, column=0, columnspan=2,rowspan=1,sticky="nsew",  padx=1, pady=1)
+label_sl_sw = Label(tab2, text="Slice \ Switch", bg="#0059b3", fg="white", padx=3,pady=3) #, anchor = "n"
+label_sl_sw.config(font=('Arial',12))
+label_sl_sw.grid(row=0, column=0, columnspan=2,rowspan=1,sticky="nsew",  padx=1, pady=(1,3))
 tab2.grid_columnconfigure(0, weight=1)
 tab2.grid_columnconfigure(1, weight=1)
 
 for col in range(1,n_switches+1):
     label_sw = Label(tab2, text="Switch " + str(col), bg="#0059b3", fg="white", padx=3,pady=3)
     label_sw.config(font=('Arial', 12))
-    label_sw.grid(row=0, column=col+1, sticky="nsew", columnspan=1,rowspan=1, padx=1, pady=1)
+    label_sw.grid(row=0, column=col+1, sticky="nsew", columnspan=1,rowspan=1, padx=1, pady=(1,3))
     tab2.grid_columnconfigure(col+1, weight=1)
 
 for slice in range(1,n_slices+1):
